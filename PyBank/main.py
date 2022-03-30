aa037f9d46501c4b4c1dbf9291b99add40763c1b
tylerhill122 <98857284+tylerhill122@users.noreply.github.com>
2022-03-30 14:46:23 -0400

    counting number of months

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,16 +1,23 @@
 import os
-
 import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
+date = []
+profit = []
+
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
+    # print(csvreader)
     
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    # print(f"CSV Header: {csv_header}")
     
     for row in csvreader:
-        print(row)
+        date.append(row[0])
+        profit.append(row[1])
+        months = len(date)
+    print(months)
+        
+        
 
