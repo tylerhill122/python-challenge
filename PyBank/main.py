b7e7f2a239b3884355a6d7e17a0d4fd0e57feb47
tylerhill122 <98857284+tylerhill122@users.noreply.github.com>
2022-04-01 19:27:42 -0400

    Greatest increase and Decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,45 +1,59 @@
 import os
 import csv
 
+from numpy import average
+
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 # Defining lists to store values
 date = []
 revenue = []
+profit_history = []
 
 # Open csv file
 with open(csvpath) as csvfile:
-    
-    # Define sum_revenue to add up values of revenue list
+# Declares how the csv file should be read
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+# Define sum_revenue to add up values of revenue list
     def sum_revenue(csvreader):
         total = 0
         for row in csvreader:
             total = int(total) + int(row)
-        return total      
-    
-    # Declares how the csv file should be read
-    csvreader = csv.reader(csvfile, delimiter=',')
-    #print(csvreader)
-    
-    csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
-    
-    # for loop to add values of date and revenue
+        return total
+# for loop to add values of date and revenue
     for row in csvreader:
         date.append(row[0])
-        revenue.append(row[1])
-        # count number of months in months list
+        revenue.append(row[1]) 
+
+for i in range(len(revenue)):
+    change = int(revenue[i-1]) + (1 - int(revenue[i]))
+    profit_history.append(change)
+
+# def add(i):
+#     num = 0
+#     for i in range(0, len(profit_history)):
+#         num = num + profit_history[i]
+#     return num
+
 
 months = len(date)
 total_sum = sum_revenue(revenue)
-        
+
+min = min(profit_history)
+max = max(profit_history)
+total_change = sum(profit_history)
+average_change = (max - min) / total_change
+
+# print(revenue)
+# print(profit_history)
+       
 print(
     "Financial Anaylsis \n"
     "------------------------------ \n"
     f'Total Months: {months} \n'
     f'Total: {total_sum} \n'
-    f'{date} \n'
+    f'Average Change: {average_change} \n'
+    f'Greatest Increase in Profits: {max} \n'
+    f'Greatest Decrease in Profits: {min} \n'
     )
-        
-    # print(months)
-    # print(profit)
