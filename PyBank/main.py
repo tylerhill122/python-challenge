c48a284d83edf318c38f81b530a9a0eeb117d5ac
tylerhill122 <98857284+tylerhill122@users.noreply.github.com>
2022-04-02 19:53:06 -0400

    average change calculation

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import statistics
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
@@ -28,30 +29,35 @@ for i in range(len(revenue)):
     change = int(revenue[i-1]) + (1 - int(revenue[i]))
     profit_history.append(change)
 
-# def add(i):
-#     num = 0
-#     for i in range(0, len(profit_history)):
-#         num = num + profit_history[i]
-#     return num
+# for i in range(len(revenue)):
+#     c1 = revenue[i]
+#     c2 = revenue[i-1]
+#     change = int(c1) + int(c2)
+#     profit_history.append(change)
+#     c1, c2 = 0, 0
 
+neg_profit = list(filter(lambda profit_history:profit_history<0,profit_history))
+pos_profit = list(filter(lambda profit_history:profit_history>0,profit_history))
+
+profit_change = statistics.mean(pos_profit)+statistics.mean(neg_profit)
+
+total_change = profit_change / len(profit_history)
 
 months = len(date)
 total_sum = sum_revenue(revenue)
-
 min = min(profit_history)
 max = max(profit_history)
-total_change = sum(profit_history)
-average_change = (max - min) / total_change
 
 # print(revenue)
 # print(profit_history)
        
 print(
+    "------------------------------ \n"
     "Financial Anaylsis \n"
     "------------------------------ \n"
     f'Total Months: {months} \n'
-    f'Total: {total_sum} \n'
-    f'Average Change: {average_change} \n'
-    f'Greatest Increase in Profits: {max} \n'
-    f'Greatest Decrease in Profits: {min} \n'
+    f'Total: $ {total_sum} \n'
+    f'Average Change: {total_change} \n'
+    f'Greatest Increase in Profits: $ {max} \n'
+    f'Greatest Decrease in Profits: $ {min} \n'
     )
